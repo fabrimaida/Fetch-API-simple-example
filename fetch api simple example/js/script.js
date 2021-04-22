5ccf3976658b09659f856635c3ba54a154fc382b
fabrimaida <fbrmaida@gmail.com>
2021-04-22 17:53:50 +0200

    logA

--- fetch api simple example/js/script.js
+++ fetch api simple example/js/script.js
@@ -4,6 +4,8 @@
 // If the number is even we paint the box of red.
 // If the number is odd we paint the box green.
 // Easy as that!
+alert("A")
+
 
 var board = document.getElementById('board');
 var source = document.getElementById("entry-template").innerHTML;
